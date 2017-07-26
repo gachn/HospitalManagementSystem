aa1593ae19b80218c0c4087867b80180fb2d5794
Gaurav Chauhan <chauhangaurav101@gmail.com>
2017-07-26 22:45:03 +0530

    login page added

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var login=require('./routes/login')
 var app = express();
 
 // view engine setup
@@ -24,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/login',login);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
