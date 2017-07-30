aeebd237f895f47d0cca13d9b8a28a1fb416f417
Gaurav Chauhan <chauhangaurav101@gmail.com>
2017-07-30 08:59:06 +0530

    profile added

--- routes/patientRecord.js
+++ routes/patientRecord.js
@@ -1,3 +1,13 @@
 /**
  * Created by Gaurav on 7/29/2017.
  */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    res.render('patient_record_display');
+});
+
+module.exports = router;
